abc8c3143de547b4cc752e27cb2579af442497cb
Marina Gabrielly <marina.gabri3lly@gmail.com>
2025-05-19 17:43:41 -0300

    modificacao
    19/05

---
+++ index/index.js
@@ -0,0 +1,5 @@
+const titulo = document.getElementById('titulo');
+const botaoSalvar = document.getElementById('botaoSalvar');
+const listaLivros = document.querySelector('.lista-livros'); 
+const formulario = document.querySelector('form'); 
+const inputAutor = document.querySelector('input[name="autor"]');
